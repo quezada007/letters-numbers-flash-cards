ab6f944c4eeb8a123d54c545c6568d89e43f94a2
Jose Quezada <quezada007@yahoo.com>
2019-09-23 21:10:28 -0700

    add the English and Spanish letters

---
+++ src/js/lib/letters.js
@@ -0,0 +1,220 @@
+const letters = {
+    english: [
+        {
+            upperCase: 'A',
+            lowerCase: 'a'
+        },
+        {
+            upperCase: 'B',
+            lowerCase: 'b'
+        },
+        {
+            upperCase: 'C',
+            lowerCase: 'c'
+        },
+        {
+            upperCase: 'D',
+            lowerCase: 'd'
+        },
+        {
+            upperCase: 'E',
+            lowerCase: 'e'
+        },
+        {
+            upperCase: 'F',
+            lowerCase: 'f'
+        },
+        {
+            upperCase: 'G',
+            lowerCase: 'g'
+        },
+        {
+            upperCase: 'H',
+            lowerCase: 'h'
+        },
+        {
+            upperCase: 'I',
+            lowerCase: 'i'
+        },
+        {
+            upperCase: 'J',
+            lowerCase: 'j'
+        },
+        {
+            upperCase: 'K',
+            lowerCase: 'k'
+        },
+        {
+            upperCase: 'L',
+            lowerCase: 'l'
+        },
+        {
+            upperCase: 'M',
+            lowerCase: 'm'
+        },
+        {
+            upperCase: 'N',
+            lowerCase: 'n'
+        },
+        {
+            upperCase: 'O',
+            lowerCase: 'o'
+        },
+        {
+            upperCase: 'P',
+            lowerCase: 'p'
+        },
+        {
+            upperCase: 'Q',
+            lowerCase: 'q'
+        },
+        {
+            upperCase: 'R',
+            lowerCase: 'r'
+        },
+        {
+            upperCase: 'S',
+            lowerCase: 's'
+        },
+        {
+            upperCase: 'T',
+            lowerCase: 't'
+        },
+        {
+            upperCase: 'U',
+            lowerCase: 'u'
+        },
+        {
+            upperCase: 'V',
+            lowerCase: 'v'
+        },
+        {
+            upperCase: 'W',
+            lowerCase: 'w'
+        },
+        {
+            upperCase: 'X',
+            lowerCase: 'x'
+        },
+        {
+            upperCase: 'Y',
+            lowerCase: 'y'
+        },
+        {
+            upperCase: 'Z',
+            lowerCase: 'z'
+        }
+    ],
+    spanish: [
+        {
+            upperCase: 'A',
+            lowerCase: 'a'
+        },
+        {
+            upperCase: 'B',
+            lowerCase: 'b'
+        },
+        {
+            upperCase: 'C',
+            lowerCase: 'c'
+        },
+        {
+            upperCase: 'D',
+            lowerCase: 'd'
+        },
+        {
+            upperCase: 'E',
+            lowerCase: 'e'
+        },
+        {
+            upperCase: 'F',
+            lowerCase: 'f'
+        },
+        {
+            upperCase: 'G',
+            lowerCase: 'g'
+        },
+        {
+            upperCase: 'H',
+            lowerCase: 'h'
+        },
+        {
+            upperCase: 'I',
+            lowerCase: 'i'
+        },
+        {
+            upperCase: 'J',
+            lowerCase: 'j'
+        },
+        {
+            upperCase: 'K',
+            lowerCase: 'k'
+        },
+        {
+            upperCase: 'L',
+            lowerCase: 'l'
+        },
+        {
+            upperCase: 'M',
+            lowerCase: 'm'
+        },
+        {
+            upperCase: 'N',
+            lowerCase: 'n'
+        },
+        {
+            upperCase: 'Ñ',
+            lowerCase: 'ñ'
+        },
+        {
+            upperCase: 'O',
+            lowerCase: 'o'
+        },
+        {
+            upperCase: 'P',
+            lowerCase: 'p'
+        },
+        {
+            upperCase: 'Q',
+            lowerCase: 'q'
+        },
+        {
+            upperCase: 'R',
+            lowerCase: 'r'
+        },
+        {
+            upperCase: 'S',
+            lowerCase: 's'
+        },
+        {
+            upperCase: 'T',
+            lowerCase: 't'
+        },
+        {
+            upperCase: 'U',
+            lowerCase: 'u'
+        },
+        {
+            upperCase: 'V',
+            lowerCase: 'v'
+        },
+        {
+            upperCase: 'W',
+            lowerCase: 'w'
+        },
+        {
+            upperCase: 'X',
+            lowerCase: 'x'
+        },
+        {
+            upperCase: 'Y',
+            lowerCase: 'y'
+        },
+        {
+            upperCase: 'Z',
+            lowerCase: 'z'
+        }
+    ]
+};
+
+export default letters;

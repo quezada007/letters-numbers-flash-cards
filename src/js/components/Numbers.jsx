6f6aa90be50cc727aa3d3491ab3742c8392c1196
Jose Quezada <quezada007@yahoo.com>
2019-09-24 06:22:05 -0700

    add classes and remove the change language button

--- src/js/components/Numbers.jsx
+++ src/js/components/Numbers.jsx
@@ -27,20 +27,24 @@ export default class Numbers extends React.Component {
 
     render() {
         const { currentNumber } = this.state;
-        const { currentLanguage, changeLanguage } = this.props;
+        const { currentLanguage } = this.props;
+        const heading = currentLanguage === 'english' ? 'Numbers in English' : 'Numeros en Español';
         return (
-            <>
-                <div>{currentNumber}</div>
-                <div>{numbers[currentLanguage][currentNumber]}</div>
-                <button type="button" onClick={this.prevNumber}>Previous</button>
-                <button type="button" onClick={this.nextNumber}>Next</button>
-                <button type="button" onClick={() => changeLanguage()}>Change Language</button>
-            </>
+            <div className="cards cards--numbers">
+                <h1 className="cards__heading">{heading}</h1>
+                <div className="cards__card">
+                    <div className="cards__number">{currentNumber}</div>
+                    <div className="cards__number-word">{numbers[currentLanguage][currentNumber]}</div>
+                </div>
+                <div className="cards__btn__container">
+                    <button type="button" className="cards__btn cards__btn--prev" onClick={this.prevNumber} aria-label="Previous"><i className="icon-long-arrow-left" /></button>
+                    <button type="button" className="cards__btn cards__btn--next" onClick={this.nextNumber} aria-label="Next"><i className="icon-long-arrow-right" /></button>
+                </div>
+            </div>
         );
     }
 }
 
 Numbers.propTypes = {
-    currentLanguage: PropTypes.string.isRequired,
-    changeLanguage: PropTypes.func.isRequired
+    currentLanguage: PropTypes.string.isRequired
 };

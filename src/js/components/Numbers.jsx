bd4faf1b76b41521fddce29f72217d8a71e74ca3
Jose Quezada <quezada007@yahoo.com>
2019-09-24 21:33:38 -0700

    update the Numbers component

--- src/js/components/Numbers.jsx
+++ src/js/components/Numbers.jsx
@@ -29,14 +29,15 @@ export default class Numbers extends React.Component {
         const { currentNumber } = this.state;
         const { currentLanguage } = this.props;
         const heading = currentLanguage === 'english' ? 'Numbers in English' : 'Numeros en Español';
+        const digitsClass = currentNumber > 99 ? 'cards__number cards__number--3-digits' : 'cards__number';
         return (
             <div className="cards cards--numbers">
                 <h1 className="cards__heading">{heading}</h1>
                 <div className="cards__card">
-                    <div className="cards__number">{currentNumber}</div>
+                    <div className={digitsClass}>{currentNumber}</div>
                     <div className="cards__number-word">{numbers[currentLanguage][currentNumber]}</div>
                 </div>
-                <div className="cards__btn__container">
+                <div className="cards__btn-container">
                     <button type="button" className="cards__btn cards__btn--prev" onClick={this.prevNumber} aria-label="Previous"><i className="icon-long-arrow-left" /></button>
                     <button type="button" className="cards__btn cards__btn--next" onClick={this.nextNumber} aria-label="Next"><i className="icon-long-arrow-right" /></button>
                 </div>

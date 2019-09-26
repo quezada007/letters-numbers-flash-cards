03c69f31e02fb96c6edbeb274496dffd0af1501d
Jose Quezada <quezada007@yahoo.com>
2019-09-26 06:27:53 -0700

    update the App component

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,16 +1,50 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
+import Letters from './Letters';
+// import Numbers from './Numbers';
 
 class App extends React.Component {
+    language = {
+        english: 'English',
+        spanish: 'Español'
+    };
+
+    voice = {
+        english: 'Google US English',
+        spanish: 'Google español de Estados Unidos'
+    }
+
     constructor(props) {
         super(props);
 
         this.state = {
+            currentLanguage: 'english'
         };
+
+        this.msg = new SpeechSynthesisUtterance();
+        speechSynthesis.onvoiceschanged = () => this.changeVoice('english');
+    }
+
+    changeVoice = (currentLanguage) => {
+        const currentVoice = this.voice[currentLanguage];
+        [this.msg.voice] = speechSynthesis.getVoices().filter((voice) => voice.name === currentVoice);
+    }
+
+    changeLanguage = () => {
+        const { currentLanguage } = this.state;
+        this.setState({
+            currentLanguage: currentLanguage === 'english' ? 'spanish' : 'english'
+        });
+        this.changeVoice(currentLanguage);
     }
 
     render() {
+        const { currentLanguage } = this.state;
         return (
+            <>
+                <Letters currentLanguage={currentLanguage} changeLanguage={this.changeLanguage} msg={this.msg} />
+                {/* <Numbers currentLanguage={currentLanguage} changeLanguage={this.changeLanguage} msg={this.msg} /> */}
+            </>
         );
     }
 }

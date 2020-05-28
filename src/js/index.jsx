66db67523c4188e3ecee8086cfcb9850b325f4b3
Jose Quezada <quezada007@yahoo.com>
2020-05-28 08:30:27 -0700

    Add Redux store and React Redux Provider to the App component

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -4,7 +4,13 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import allReducers from './reducers';
 import App from './components/App';
+
+// eslint-disable-next-line no-underscore-dangle
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 // Check for Web Accessibility issues in development mode
 if (process.env.NODE_ENV === 'development') {
@@ -14,7 +20,12 @@ if (process.env.NODE_ENV === 'development') {
     axe(React, ReactDOM, 1000);
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('app')
+);
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
